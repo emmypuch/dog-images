078193ecd280f93f923b8a2de25017ae34b89af8
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-07-14 16:39:20 +0100

    fixed bug

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div>
     <router-view />
-    <!-- <WelcomeUser /> -->
   </div>
 </template>
 
 <script>
-// import WelcomeUser from "../src/pages/WelcomeUser.vue";
-
 export default {
   name: "App",
-  // components: {
-  //   WelcomeUser,
-  // },
 };
 </script>
 
